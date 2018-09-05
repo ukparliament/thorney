b8249a08c198dbf00a9507fe4fe8cb4b7aa6a281
Rebecca Appleyard <rklappleyard@gmail.com>
2018-09-05 12:36:26 +0100

    Added ListDescriptionComponentSerializer
    * Added serializer and tests.
    * Updated documentation.

---
+++ spec/serializers/component_serializer/list_description_component_serializer_spec.rb
@@ -0,0 +1,56 @@
+require_relative '../../rails_helper'
+
+RSpec.describe ComponentSerializer::ListDescriptionComponentSerializer do
+  context '#to_h' do
+    context 'returns a hash containing the name and data' do
+      it 'when items is specified' do
+        serializer = described_class.new(
+          "items":
+            [{
+               "term":
+                   {
+                     "content": "telephone"
+                   },
+               "description":
+                 [
+                   {
+                     "content": "012349586"
+                   }
+                 ]
+             },
+             {
+               "term":
+                   {
+                     "content": "website"
+                   },
+               "description":
+                 [
+                   {
+                     "content": "<a href='www.example.com'>My home page</a>"
+                   }
+                 ]
+             },
+             {
+               "term":
+                   {
+                     "content": "email"
+                   },
+               "description":
+                 [
+                   {
+                     "content": "hello@example.com"
+                   },
+                   {
+                     "content": "hello@gmail.com"
+                   }
+                 ]
+             }
+            ])
+
+        expected   = get_fixture('description')
+
+        expect(serializer.to_yaml).to eq expected
+      end
+    end
+  end
+end
