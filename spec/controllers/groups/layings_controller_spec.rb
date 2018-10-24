10295492a7d14f1b953c685aa82f759ce3d0e678
Allan Wazacz <cazwazacz@gmail.com>
2018-10-24 12:02:59 +0100

    [WEBDEV-877][WEBDEV-878][WEBDEV-879] Update Group Layings page

--- spec/controllers/groups/layings_controller_spec.rb
+++ spec/controllers/groups/layings_controller_spec.rb
@@ -1,24 +1,24 @@
-# -*- coding: utf-8 -*-
 require 'rails_helper'
 
 RSpec.describe Groups::LayingsController, vcr: true do
   describe 'GET index' do
     let(:data_alternates) do
       [{
-           :href => "#{ENV['PARLIAMENT_BASE_URL']}/group_layings_index.nt?group_id=XouN12Ow",
-           :type => "application/n-triples" },
-       { :href => "#{ENV['PARLIAMENT_BASE_URL']}/group_layings_index.ttl?group_id=XouN12Ow",
-         :type => "text/turtle" },
-       { :href => "#{ENV['PARLIAMENT_BASE_URL']}/group_layings_index.tsv?group_id=XouN12Ow",
-         :type => "text/tab-separated-values" },
-       { :href => "#{ENV['PARLIAMENT_BASE_URL']}/group_layings_index.csv?group_id=XouN12Ow",
-         :type => "text/csv" },
-       { :href => "#{ENV['PARLIAMENT_BASE_URL']}/group_layings_index.rj?group_id=XouN12Ow",
-         :type => "application/json+rdf" },
-       { :href => "#{ENV['PARLIAMENT_BASE_URL']}/group_layings_index.json?group_id=XouN12Ow",
-         :type => "application/json+ld" },
-       { :href => "#{ENV['PARLIAMENT_BASE_URL']}/group_layings_index.xml?group_id=XouN12Ow",
-         :type => "application/rdf+xml" }]
+        href: "#{ENV['PARLIAMENT_BASE_URL']}/group_layings_index.nt?group_id=XouN12Ow",
+        type: 'application/n-triples'
+      },
+       { href: "#{ENV['PARLIAMENT_BASE_URL']}/group_layings_index.ttl?group_id=XouN12Ow",
+         type: 'text/turtle' },
+       { href: "#{ENV['PARLIAMENT_BASE_URL']}/group_layings_index.tsv?group_id=XouN12Ow",
+         type: 'text/tab-separated-values' },
+       { href: "#{ENV['PARLIAMENT_BASE_URL']}/group_layings_index.csv?group_id=XouN12Ow",
+         type: 'text/csv' },
+       { href: "#{ENV['PARLIAMENT_BASE_URL']}/group_layings_index.rj?group_id=XouN12Ow",
+         type: 'application/json+rdf' },
+       { href: "#{ENV['PARLIAMENT_BASE_URL']}/group_layings_index.json?group_id=XouN12Ow",
+         type: 'application/json+ld' },
+       { href: "#{ENV['PARLIAMENT_BASE_URL']}/group_layings_index.xml?group_id=XouN12Ow",
+         type: 'application/rdf+xml' }]
     end
 
     let(:heading) { 'a heading component' }
@@ -27,7 +27,7 @@ RSpec.describe Groups::LayingsController, vcr: true do
       allow(PageSerializer::ListPageSerializer).to receive(:new)
       allow(ComponentSerializer::Heading1ComponentSerializer).to receive(:new) { heading }
 
-      allow(controller.request).to receive(:env).and_return({'ApplicationInsights.request.id' => '|1234abcd.'})
+      allow(controller.request).to receive(:env).and_return({ 'ApplicationInsights.request.id' => '|1234abcd.' })
 
       get :index, params: { group_id: 'XouN12Ow' }
     end
@@ -56,7 +56,82 @@ RSpec.describe Groups::LayingsController, vcr: true do
       end
 
       it 'calls the ListPageSerializer correctly' do
-        list_components = [{"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 1", "size"=>2, "link"=>"/statutory-instruments/QV9rpep6"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"27 April 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 2", "size"=>2, "link"=>"/statutory-instruments/HJ7BlYte"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"8 May 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 3", "size"=>2, "link"=>"/statutory-instruments/3dAYwLoB"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"3 April 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 4", "size"=>2, "link"=>"/statutory-instruments/rjzMGG28"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"21 May 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 5", "size"=>2, "link"=>"/statutory-instruments/uNiGNHey"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"24 May 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 6", "size"=>2, "link"=>"/statutory-instruments/8HUWJm9e"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"18 May 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 7", "size"=>2, "link"=>"/statutory-instruments/ez3sdNNT"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"4 June 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 8", "size"=>2, "link"=>"/statutory-instruments/w3m931qg"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"6 June 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 9", "size"=>2, "link"=>"/statutory-instruments/PhwmELqG"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"8 June 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 10", "size"=>2, "link"=>"/statutory-instruments/aQaZuLyK"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"13 June 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 11", "size"=>2, "link"=>"/statutory-instruments/I9rWxORS"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"13 June 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 12", "size"=>2, "link"=>"/statutory-instruments/9Nv0gELe"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"15 June 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 13", "size"=>2, "link"=>"/statutory-instruments/YhMNMpYo"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"18 June 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 14", "size"=>2, "link"=>"/statutory-instruments/UD4HjKbu"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"25 June 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 15", "size"=>2, "link"=>"/statutory-instruments/P5Zu6fkP"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"28 June 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 16", "size"=>2, "link"=>"/statutory-instruments/JXxhQblu"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"3 July 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 17", "size"=>2, "link"=>"/statutory-instruments/IjLgnN8z"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"2 July 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 18", "size"=>2, "link"=>"/statutory-instruments/BRZBy463"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"12 July 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 19", "size"=>2, "link"=>"/statutory-instruments/Uhsvsfdn"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"19 July 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 20", "size"=>2, "link"=>"/statutory-instruments/SbxmGLcR"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"20 July 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 21", "size"=>2, "link"=>"/proposed-negative-statutory-instruments/W3l3iqIJ"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"25 July 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Proposed Negative Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 22", "size"=>2, "link"=>"/statutory-instruments/hLF7ZU6f"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"26 July 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 23", "size"=>2, "link"=>"/statutory-instruments/BuMKAKjA"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"6 September 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 24", "size"=>2, "link"=>"/statutory-instruments/WBlaNvyq"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"10 September 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 25", "size"=>2, "link"=>"/statutory-instruments/lSRBYtGi"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"13 September 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}, {"name"=>"card__generic", "data"=>{"heading"=>{"name"=>"heading", "data"=>{"content"=>"laidThingName - 26", "size"=>2, "link"=>"/statutory-instruments/H1uGO0VI"}}, "paragraph"=>{"name"=>"paragraph", "data"=>[{"content"=>"groups.layings.date", "data"=>{"date"=>"13 September 2018"}}, {"content"=>"groups.layings.type", "data"=>{"type"=>"Statutory Instrument"}}]}}}]
+        list_components = [{ 'data' =>
+                                       { 'heading'          =>
+                                                               { 'data' =>
+                                                                           { 'content' =>
+                                                                                          'laidThingName - 24',
+                                                                             'link' =>
+                                                                                 '/statutory-instruments/WBlaNvyq',
+                                                                             'size'    => 2 },
+                                                                 'name' => 'heading' },
+                                         'list-description' =>
+                                                               { 'data' =>
+                                                                           { 'items' =>
+                                                                                        [{ 'description' => [{ 'content' => '10 September 2018' }],
+                                                                                           'term'        => { 'content' => 'laid-thing.laid-date' } }] },
+                                                                 'name' => 'list__description' },
+                                         'small'            =>
+                                                               { 'data' => { 'content' => 'Statutory Instrument' },
+                                                                 'name' => 'partials__small' } },
+                             'name' => 'card__generic' },
+                           { 'data' =>
+                                       { 'heading'          =>
+                                                               { 'data' =>
+                                                                           { 'content' =>
+                                                                                          'laidThingName - 23',
+                                                                             'link' =>
+                                                                                 '/statutory-instruments/BuMKAKjA',
+                                                                             'size'    => 2 },
+                                                                 'name' => 'heading' },
+                                         'list-description' =>
+                                                               { 'data' =>
+                                                                           { 'items' =>
+                                                                                        [{ 'description' => [{ 'content' => '6 September 2018' }],
+                                                                                           +'term'       => { 'content' => 'laid-thing.laid-date' } }] },
+                                                                 'name' => 'list__description' },
+                                         'small'            =>
+                                                               { 'data' => { 'content' => 'Statutory Instrument' },
+                                                                 'name' => 'partials__small' } },
+                             'name' => 'card__generic' },
+                           { 'data' =>
+                                       { 'heading'          =>
+                                                               { 'data' =>
+                                                                           { 'content' =>
+                                                                                          'laidThingName - 21',
+                                                                             'link' =>
+                                                                                 '/proposed-negative-statutory-instruments/W3l3iqIJ',
+                                                                             'size'    => 2 },
+                                                                 'name' => 'heading' },
+                                         'list-description' =>
+                                                               { 'data' =>
+                                                                           { 'items' =>
+                                                                                        [{ 'description' => [{ 'content' => '25 July 2018' }],
+                                                                                           'term'        => { 'content' => 'laid-thing.laid-date' } }] },
+                                                                 'name' => 'list__description' },
+                                         'small'            =>
+                                                               { 'data' => { 'content' => 'Proposed Negative Statutory Instrument' },
+                                                                 'name' => 'partials__small' } },
+                             'name' => 'card__generic' },
+                           { 'data' =>
+                                       { 'heading'          =>
+                                                               { 'data' =>
+                                                                           { 'content' =>
+                                                                                          'laidThingName - 1',
+                                                                             'link' =>
+                                                                                 '/statutory-instruments/QV9rpep6',
+                                                                             'size'    => 2 },
+                                                                 'name' => 'heading' },
+                                         'list-description' =>
+                                                               { 'data' =>
+                                                                           { 'items' =>
+                                                                                        [{ 'description' => [{ 'content' => '27 April 2018' }],
+                                                                                           'term'        => { 'content' => 'laid-thing.laid-date' } }] },
+                                                                 'name' => 'list__description' },
+                                         'small'            =>
+                                                               { 'data' => { 'content' => 'Statutory Instrument' },
+                                                                 'name' => 'partials__small' } },
+                             'name' => 'card__generic' }]
 
         expect(PageSerializer::ListPageSerializer).to have_received(:new).with(request: request, heading_component: heading, list_components: list_components, data_alternates: data_alternates)
       end
