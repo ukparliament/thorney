861d900f8300db8a00b4af3d95601b171d91f7e4
Christine-horrocks <splogy2001@hotmail.com>
2018-11-05 13:06:22 +0000

    Rubocop changes

--- app/controllers/concerns/content_data_helper.rb
+++ app/controllers/concerns/content_data_helper.rb
@@ -4,9 +4,8 @@ module ContentDataHelper
   # The data hash contains key/value pairs where the key is also the key inside curly braces in the string to be interpolated. The values are the data from the database which will be inserted into the string.
   # Should this be called the data interpolation helper??
   def self.content_data(content: nil, **data)
-
     {}.tap do |hash|
-      hash[:content] = content if content
+      hash[:content] = content
       hash[:data] = data if data != {}
     end
   end
