1b5c886b4fdb0affe7258996048fda7f4da7d1d4
Christine-horrocks <splogy2001@hotmail.com>
2018-11-05 11:31:08 +0000

    First commit

---
+++ app/controllers/concerns/content_data_helper.rb
@@ -0,0 +1,13 @@
+module ContentDataHelper
+  # Helper method to create hash for any part of a component or partial which needs a content and data hash to send through a translation key with multiple data items.
+  # The content value is the translation key.
+  # The data hash contains key/value pairs where the key is also the key inside curly braces in the string to be interpolated. The values are the data from the database which will be inserted into the string.
+  # Should this be called the data interpolation helper??
+  def content_data_(content: nil, **data)
+
+    {}.tap do |hash|
+      hash[:content] = content if content
+      hash[:data] = data if data
+    end
+  end
+end
