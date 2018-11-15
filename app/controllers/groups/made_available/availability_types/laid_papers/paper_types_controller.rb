2ebd4d3bcda558d782149e2785bc7c766087acec
Rebecca Appleyard <rklappleyard@gmail.com>
2018-11-15 14:13:11 +0000

    [WEBDEV-986] Added groups laid papers by paper type
    * Added controller Groups::MadeAvailable::AvailabilityTypes::LaidPapers::PaperTypesController

---
+++ app/controllers/groups/made_available/availability_types/laid_papers/paper_types_controller.rb
@@ -0,0 +1,64 @@
+module Groups
+  module MadeAvailable
+    module AvailabilityTypes
+      module LaidPapers
+        class PaperTypesController < ApplicationController
+          # Controller rendering groups/made-available/availability-types/laid-papers/paper-types
+          before_action :data_check, :build_request
+
+          ROUTE_MAP = {
+            index: proc { |params| ParliamentHelper.parliament_request.group_by_id.set_url_params({ group_id: params[:group_id] }) },
+            show:  proc do |params|
+              request = ParliamentHelper.parliament_request
+              paper_type = params.fetch(:paper_type)
+
+              request.group_laid_papers_paper_type_statutory_instruments.set_url_params({ group_id: params[:group_id] }) if paper_type == 'statutory-instruments'
+              request.group_laid_papers_paper_type_proposed_negative_statutory_instruments.set_url_params({ group_id: params[:group_id] }) if paper_type == 'proposed-negative-statutory-instruments'
+
+              request
+            end
+          }.freeze
+
+          def index
+            @group = FilterHelper.filter(@api_request, 'Group')
+            @group = @group.first
+
+            list_components = [
+              CardFactory.new(
+                heading_text: I18n.t('proposed_negative_statutory_instruments.type').pluralize,
+                heading_url:  group_made_available_availability_types_laid_papers_paper_type_path(group_id: @group.graph_id, paper_type: 'proposed-negative-statutory-instruments')
+              ).build_card,
+              CardFactory.new(
+                heading_text: I18n.t('statutory_instruments.type').pluralize,
+                heading_url:  group_made_available_availability_types_laid_papers_paper_type_path(group_id: @group.graph_id, paper_type: 'statutory-instruments')
+              ).build_card
+            ]
+
+            heading = ComponentSerializer::Heading1ComponentSerializer.new(heading: I18n.t('groups.made_available.availability_types.paper_types.index.title', group: @group.try(:groupName)), subheading: @group.try(:groupName), subheading_link: group_path)
+
+            serializer = PageSerializer::ListPageSerializer.new(request: request, heading_component: heading, list_components: list_components, data_alternates: @alternates)
+
+            render_page(serializer)
+          end
+
+          def show
+            @group, @laid_things = FilterHelper.filter(@api_request, 'Group', 'LaidThing')
+            @group = @group.first
+
+            list_components = LaidThingListComponentsFactory.sort_and_build_components(statutory_instruments: @laid_things, type: :laid_thing)
+
+            paper_type          = params.fetch(:paper_type)
+            heading_translation = 'groups.made_available.availability_types.paper_types.show.si_title' if paper_type == 'statutory-instruments'
+            heading_translation = 'groups.made_available.availability_types.paper_types.show.pnsi_title' if paper_type == 'proposed-negative-statutory-instruments'
+
+            heading = ComponentSerializer::Heading1ComponentSerializer.new(heading: I18n.t(heading_translation, group: @group.try(:groupName)), subheading: @group.try(:groupName), subheading_link: group_path)
+
+            serializer = PageSerializer::ListPageSerializer.new(request: request, heading_component: heading, list_components: list_components, data_alternates: @alternates)
+
+            render_page(serializer)
+          end
+        end
+      end
+    end
+  end
+end
